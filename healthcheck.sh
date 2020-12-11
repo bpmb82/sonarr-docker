2bcb9c4feca0c04827774027d5f54b5c69b13537
bpmb82 <56978424+bpmb82@users.noreply.github.com>
2020-12-11 21:50:53 +0100

    Update healthcheck.sh

--- healthcheck.sh
+++ healthcheck.sh
@@ -4,5 +4,5 @@ FILE=/config/config.xml
 
 until test -f $FILE; do sleep 1; done
 
-API=$(xml_grep --text_only 'ApiKey' /config/config.xml)
+API=$(xml_grep --text_only 'ApiKey' $FILE)
 curl -f "http://localhost:8989/api/system/status?apikey=$API" || exit 1
