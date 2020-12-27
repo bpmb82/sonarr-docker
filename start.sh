ffe4a1b6002fc890d3e87d5cd441fd03d5dc12bc
bpmb82 <56978424+bpmb82@users.noreply.github.com>
2020-12-28 00:18:21 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $ENABLE_BACKUP = "1" ]; then
-  /app/sonarr/bin/backup.sh &&
+  /app/sonarr/bin/backup.sh &
 fi
 
 mono --debug /app/sonarr/bin/Sonarr.exe -nobrowser -data=/config
