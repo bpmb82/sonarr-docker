fc5f56d490552b55698787976e496d8c61613540
bpmb82 <56978424+bpmb82@users.noreply.github.com>
2020-12-28 18:28:10 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -4,6 +4,12 @@
 
 APP=sonarr
 
+# Only start running if db file is found
+
+FILE=/config/$APP.db
+
+until test -f $FILE; do echo "SQLite Backup Script [Info] No database file detected yet, sleeping for 5 minutes.." >> /proc/1/fd/1 && sleep 300; done
+
 # Loop forever and sleep 24 hours after each backup
 
 while true
@@ -26,10 +32,10 @@ OUTPUTFILE=$DESTINATION/$BACKUPFILE
 # Backup actual db file locally in /backups
 
 if test -f "$OUTPUTFILE"; then
-  echo "$DATETIME SQLite Backup Script | File exists already, skipping backup.." >> /proc/1/fd/1
+  echo "$DATETIME SQLite Backup Script [Info] File exists already, skipping backup.." >> /proc/1/fd/1
 else
   sqlite3 $SOURCE ".backup $DESTINATION/$BACKUPFILE"
-  echo "$DATETIME SQLite Backup Script | Backup made successfully.." >> /proc/1/fd/1
+  echo "$DATETIME SQLite Backup Script [Info] Backup made successfully.." >> /proc/1/fd/1
 fi
 
 # Keep last 30 backups
@@ -37,9 +43,9 @@ fi
 if [ $NO_BACKUPS -gt 30 ]; then
   REMOVEFILE=$(ls -t $DESTINATION | tail -1)
   rm -f $DESTINATION$REMOVEFILE
-  echo "$DATETIME SQLite Backup Script | Removed backups from more than 30 days ago.." >> /proc/1/fd/1
+  echo "$DATETIME SQLite Backup Script [Info] Removed backups from more than 30 days ago.." >> /proc/1/fd/1
 else
-  echo "$DATETIME SQLite Backup Script | We now have $NO_BACKUPS backups so nothing to remove!" >> /proc/1/fd/1
+  echo "$DATETIME SQLite Backup Script [Info] We now have $NO_BACKUPS backups so nothing to remove!" >> /proc/1/fd/1
 fi
 
 sleep 86400
